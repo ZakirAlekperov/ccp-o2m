25bee8e076824ba308b5660ed9264db25c473c2f
ZakirAlekperov <zakiralekperov1998@gmail.com>
2026-04-14 13:40:00 +0300

    fix: register UserViewSet with 'users' prefix so actions resolve correctly

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 
 router = DefaultRouter()
-router.register(r'', UserViewSet, basename='users')
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
